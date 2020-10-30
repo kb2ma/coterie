92dcfe6aea3cd7e1171cfe6ba792076a6116640b
Ken Bannister <kb2ma@runbox.com>
2020-10-30 10:46:53 -0400

    Add ability to send a client request.
    Request is hard coded, and triggered from simple stdin command.

--- src/libcoap.nim
+++ src/libcoap.nim
@@ -12,6 +12,8 @@ type
   CProto* = uint8
   ## coap_proto_t
 
+  CTxid* = cint
+
 const
   COAP_PROTO_NONE* = 0.CProto
   COAP_PROTO_UDP* = 1.CProto
@@ -19,6 +21,7 @@ const
   COAP_PROTO_TCP* = 3.CProto
   COAP_PROTO_TLS* = 4.CProto
 
+  # Message types
   COAP_MESSAGE_CON* = 0'u8
   COAP_MESSAGE_NON* = 1'u8
   COAP_MESSAGE_ACK* = 2'u8
@@ -26,9 +29,13 @@ const
 
   COAP_RESPONSE_CODE_205* = ((2 shl 5) or 5).uint8
 
+  COAP_OPTION_URI_PATH* = 11'u16
+
+  COAP_INVALID_TXID* = -1.CTxid
+
 type
-  CRequestType* = enum
-    ## coap_request_t
+  CRequestCode* = enum
+    ## coap_request_t, and value is 'detail' portion of request method code
     COAP_REQUEST_GET = 1,
     COAP_REQUEST_POST,
     COAP_REQUEST_PUT,
@@ -53,6 +60,7 @@ type
 
   CContext* = ptr object
     ## libcoap top-level data object; libcoap always manages heap memory
+    #response_handler*: pointer
 
   CEndpoint* = ptr object
     ## libcoap coap_endpoint_t; opaque
@@ -66,13 +74,18 @@ type
     `type`*: uint8
     code*: uint8
 
+  COptlist* = ptr object
+
   CCoapBinary* = ptr object
 
   CCoapString* = ptr object
 
-  CMethodHandler* = proc (context: CContext, resource: CResource,
-                             session: CSession, req: CPdu, token: CCoapBinary,
-                             query: CCoapString, resp: CPdu) {.noconv.}
+  CRequestHandler* = proc (context: CContext, resource: CResource,
+                           session: CSession, req: CPdu, token: CCoapBinary,
+                           query: CCoapString, resp: CPdu) {.noconv.}
+
+  CResponseHandler* = proc (context: CContext, session: CSession, sent: CPdu,
+                            received: CPdu, id: CTxid) {.noconv.}
 
 
 {.push dynlib: libName.}
@@ -82,30 +95,63 @@ proc freeContext*(context: CContext) {.importc: "coap_free_context".}
 proc newContext*(listen_addr: ptr CSockAddr): CContext
      {.importc: "coap_new_context".}
 
-# session.h
+# Must include header pragma because library function is 'static inline'.
+proc registerResponseHandler*(context: CContext, handler: CResponseHandler)
+                              {.header: "coap2/net.h",
+                               importc: "coap_register_response_handler".}
+
+proc send*(session: CSession, pdu: CPdu): CTxid {.importc: "coap_send".}
+
+# coap_session.h
 proc freeEndpoint*(ep: CEndpoint) {.importc: "coap_free_endpoint".}
 
+proc maxSessionPduSize*(session: CSession): csize_t
+                       {.importc: "coap_session_max_pdu_size".}
+
+proc newClientSession*(context: CContext, local_addr: ptr CSockAddr,
+                       server_addr: ptr CSockAddr, proto: CProto): CSession
+                       {.importc: "coap_new_client_session".}
+
 proc newEndpoint*(context: CContext, listen_addr: ptr CSockAddr,
-                 proto: CProto): CEndpoint {.importc: "coap_new_endpoint"}
+                 proto: CProto): CEndpoint {.importc: "coap_new_endpoint".}
+
+proc releaseSession*(session: CSession) {.importc: "coap_session_release".}
 
 # resource.h
 proc addResource*(context: CContext, resource: CResource)
-                 {.importc: "coap_add_resource"}
+                 {.importc: "coap_add_resource".}
 
 proc initResource*(uri_path: CStringConst, flags: cint): CResource
                   {.importc: "coap_resource_init".}
 
-proc registerHandler*(resource: CResource, mthd: CRequestType,
-                     handler: CMethodHandler)
+proc registerHandler*(resource: CResource, `method`: CRequestCode,
+                     handler: CRequestHandler)
                      {.importc: "coap_register_handler".}
 
 # pdu.h
 proc addData*(pdu: CPdu, len: csize_t, data: cstring): cint
-             {.importc: "coap_add_data"}
+             {.importc: "coap_add_data".}
+
+proc deletePdu*(pdu: CPdu) {.importc: "coap_delete_pdu".}
+
+proc initPdu*(`type`: uint8, code: uint8, txid: uint16 = 0, size: csize_t): CPdu
+              {.importc: "coap_pdu_init".}
+  ## 'type' is one of the COAP_MESSAGE... constants
+  ## 'code' param value is CRequestCode for a request
+
+# option.h
+proc addOptlistPdu*(pdu: CPdu, chain: ptr COptlist): cint
+                    {.importc: "coap_add_optlist_pdu".}
+
+proc deleteOptlist*(list: COptlist) {.importc: "coap_delete_optlist".}
+  ## Must delete optlist created by newOptlist
+
+proc newOptlist*(number: uint16, length: csize_t, data: ptr uint8): COptlist
+                 {.importc: "coap_new_optlist".}
 
 # coap_io.h
 proc processIo*(context: CContext, timeout: uint32): cint
-               {.importc: "coap_io_process"}
+               {.importc: "coap_io_process".}
 
 # address.h
 proc initAddress*(address: ptr CSockAddr) {.importc: "coap_address_init".}
--- src/main.nim
+++ src/main.nim
@@ -1,26 +1,33 @@
 ## coterie main
 ##
-## Runs libcoap server
+## Runs libcoap server and client
 ##
 ## Copyright 2020 Ken Bannister
 ## SPDX-License-Identifier: Apache-2.0
 
 import libcoap, posix, nativesockets
+import threadpool
 import logging
 
-proc hndHi(context: CContext, resource: CResource, session: CSession,
-           req: CPdu, token: CCoapBinary, query: CCoapString, resp: CPdu)
-           {.exportc: "hnd_hi", noconv.} =
-  ## /hi GET handler; greeting
+var
+  log: FileLogger
+  ctx: CContext
+  address: ptr CSockAddr
+  ep: CEndpoint
+
+proc handleHi(context: CContext, resource: CResource, session: CSession,
+              req: CPdu, token: CCoapBinary, query: CCoapString, resp: CPdu)
+              {.exportc: "hnd_hi", noconv.} =
+  ## server /hi GET handler; greeting
   resp.`type` = COAP_MESSAGE_ACK
   resp.code = COAP_RESPONSE_CODE_205
   discard addData(resp, 5, "Howdy")
 
-var
-  oplog: FileLogger
-  ctx: CContext
-  address: ptr CSockAddr
-  ep: CEndpoint
+proc handleResponse(context: CContext, session: CSession, sent: CPdu,
+                    received: CPdu, id: CTxid) {.exportc: "hnd_response",
+                    noconv.} =
+  ## client response handler
+  debug("Response received")
 
 proc onQuit() {.noconv.} =
   ## Cleanup resources when exit
@@ -30,15 +37,67 @@ proc onQuit() {.noconv.} =
     dealloc(address)
   if ctx != nil:
     freeContext(ctx)
-  oplog.log(lvlInfo, "Exiting gracefully")
+  info("Exiting gracefully")
+
+
+proc sendMessage() =
+  ## Send a message to exercise client flow
+
+  # init server address/port
+  address = create(CSockAddr)
+  initAddress(address)
+  try:
+    let info = getAddrInfo("127.0.0.1", 5685.Port, Domain.AF_INET,
+                           SockType.SOCK_DGRAM, Protocol.IPPROTO_UDP)
+    address.`addr`.sin = cast[SockAddr_in](info.ai_addr[])
+    freeaddrinfo(info)
+  except OSError:
+    error("Can't resolve server address")
+    return
+
+  #init session
+  var session = newClientSession(ctx, nil, address, COAP_PROTO_UDP)
+  if session == nil:
+    error("Can't create client session")
+    return
+
+  # init PDU, including type and code
+  var pdu = initPdu(COAP_MESSAGE_CON, COAP_REQUEST_GET.uint8, 1000'u16,
+                    maxSessionPduSize(session))
+  if pdu == nil:
+    error("Can't create client PDU")
+    releaseSession(session)
+    return
+
+  # add Uri-Path option
+  var optlist = newOptlist(COAP_OPTION_URI_PATH, 4.csize_t, cast[ptr uint8]("time"))
+  if optlist == nil:
+    error("Can't create option list")
+    return
+  if addOptlistPdu(pdu, addr(optlist)) == 0:
+    error("Can't create option list")
+    deleteOptlist(optlist)
+    releaseSession(session)
+    deletePdu(pdu)
+    return
+  deleteOptlist(optlist)
+
+  # send
+  if send(session, pdu) == COAP_INVALID_TXID:
+    deletePdu(pdu)
+
+  # clean up
+  #releaseSession(session)
+
 
 # application setup
-oplog = newFileLogger("ops.log", fmtStr="[$time] $levelname: ", bufSize=0)
+log = newFileLogger("ops.log", fmtStr="[$time] $levelname: ", bufSize=0)
 addQuitProc(onQuit)
 
 # init context, listen port/endpoint
 ctx = newContext(nil)
 
+# CoAP server setup
 address = create(CSockAddr)
 initAddress(address)
 address.`addr`.sin.sin_family = Domain.AF_INET.cushort
@@ -46,13 +105,27 @@ address.`addr`.sin.sin_port = posix.htons(5683)
 
 ep = newEndpoint(ctx, address, COAP_PROTO_UDP)
 
-oplog.log(lvlInfo, "Coterie started on port ",
-          posix.ntohs(address.`addr`.sin.sin_port))
+info("Coterie started on port ", posix.ntohs(address.`addr`.sin.sin_port))
 
-var r = initResource(makeStringConst("hi"), 0.cint)
-registerHandler(r, COAP_REQUEST_GET, hndHi)
+# Establish server resources and request handlers, and also the client
+# response handler.
+var r = initResource(makeStringConst("hi"), 0)
+registerHandler(r, COAP_REQUEST_GET, handleHi)
 addResource(ctx, r)
+#ctx.response_handler = handleResponse
+registerResponseHandler(ctx, handleResponse)
 
-# serve forever
+# wait asynchronously for user input
+var messageFlowVar = spawn stdin.readLine()
+
+# Serve resources until quit
 while true:
-    discard processIo(ctx, 0'u32)
+  discard processIo(ctx, 500'u32)
+
+  if messageFlowVar.isReady():
+    case ^messageFlowVar
+    of "s":
+      sendMessage()
+    of "q":
+      break
+    messageFlowVar = spawn stdin.readLine()
